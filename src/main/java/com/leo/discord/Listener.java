54fd72df89f9a14eec68bd7234eb5a2b87b1490d
Leonard <leowa444@gmail.com>
2020-03-29 16:51:50 +0200

    added CommandManager added PingCommand

---
+++ src/main/java/com/leo/discord/Listener.java
@@ -0,0 +1,49 @@
+package com.leo.discord;
+
+import me.duncte123.botcommons.BotCommons;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+
+public class Listener extends ListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
+    private final CommandManager manager = new CommandManager();
+
+    @Override
+    public void onReady(@Nonnull ReadyEvent event) {
+        LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
+    }
+
+    @Override
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+
+        User user = event.getAuthor();
+
+        if(user.isBot() || event.isWebhookMessage()){
+            return;
+        }
+
+        String prefix = Config.get("prefix");
+        String raw = event.getMessage().getContentRaw();
+
+        if(raw.equalsIgnoreCase(prefix + "shutdown")
+                && user.getId().equals(Config.get("owner_id"))){
+            LOGGER.info("Shutting down");
+            event.getJDA().shutdown();
+            BotCommons.shutdown(event.getJDA());
+
+            return;
+        }
+
+        if(raw.startsWith(prefix)){
+            manager.handle(event);
+        }
+    }
+}
