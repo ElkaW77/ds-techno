40f3da47e352b40494fbb67724a8de2010c62485
Leonard <leowa444@gmail.com>
2020-03-29 14:43:06 +0200

    deps added logback.xml added

--- src/main/java/com/leo/discord/Main.java
+++ src/main/java/com/leo/discord/Main.java
@@ -1,9 +1,12 @@
 package com.leo.discord;
 
+import javafx.fxml.LoadException;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world2");
+
     }
 
 }
